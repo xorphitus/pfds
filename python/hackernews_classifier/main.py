c0d3c7115d05329de075c98c497c82e8b8fe45b7
xorphitus <xorphitus@gmail.com>
2017-04-24 01:14:55 +0900

    Python 結果を表示して感覚に合ってるかチェック

--- python/hackernews_classifier/main.py
+++ python/hackernews_classifier/main.py
@@ -1,3 +1,5 @@
+# pip install numpy scipy scikit-learn
+
 import csv
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
@@ -27,13 +29,18 @@ stop_words = ["'", '"', '`', '.', ',', '-', '!', '?', ':', ';', '(', ')', '*', '
 vectorizer = CountVectorizer(stop_words=stop_words)
 vectorizer.fit(X_train)
 
-X_train = vectorizer.transform(X_train)
-print(X_train.shape)
+X_train_vec = vectorizer.transform(X_train)
+print(X_train_vec.shape)
 
 clf = MultinomialNB()
-clf.fit(X_train, y_train)
-print(clf.score(X_train,y_train))
+clf.fit(X_train_vec, y_train)
+print(clf.score(X_train_vec ,y_train))
 
 # test
-X_test = vectorizer.transform(X_test)
-print(clf.score(X_test, y_test))
+X_test_vec = vectorizer.transform(X_test)
+print(clf.score(X_test_vec, y_test))
+
+print('-----')
+for i in range(0, 100):
+    print(X_test[i])
+    print(clf.predict(X_test_vec[i:i+1]))
