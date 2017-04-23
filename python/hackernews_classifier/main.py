da65fa699717e02adc164759b30ba805a3ef1472
xorphitus <xorphitus@gmail.com>
2017-04-24 01:06:39 +0900

    Python 教師データとテストデータを分けた

--- python/hackernews_classifier/main.py
+++ python/hackernews_classifier/main.py
@@ -2,15 +2,25 @@ import csv
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 
+datalen = 200
+
 X_train = []
 y_train = []
+X_test = []
+y_test = []
 
 with open('./dataset.tsv') as f:
     tsv = csv.reader(f, delimiter = '\t')
 
+    i = 0
     for row in tsv:
-      y_train.append(row[0])
-      X_train.append(row[1])
+      if i < datalen:
+          y_train.append(row[0])
+          X_train.append(row[1])
+      else:
+          y_test.append(row[0])
+          X_test.append(row[1])
+      i = i + 1
 
 stop_words = ["'", '"', '`', '.', ',', '-', '!', '?', ':', ';', '(', ')', '*', '--', '\\']
 
@@ -23,3 +33,7 @@ print(X_train.shape)
 clf = MultinomialNB()
 clf.fit(X_train, y_train)
 print(clf.score(X_train,y_train))
+
+# test
+X_test = vectorizer.transform(X_test)
+print(clf.score(X_test, y_test))
